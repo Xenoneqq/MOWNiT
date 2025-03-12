68a986f2d6f2f009aaa34b1002aade8ad6dcf31b
Xenoneqq <realfatboimlg@gmail.com>
2025-03-12 12:58:15 +0100

    Added stuff

--- solutions/lab2/solution.py
+++ solutions/lab2/solution.py
@@ -8,17 +8,21 @@ train_data = pd.read_csv("dataset/breast-cancer-train.dat", names=columns)
 validate_data = pd.read_csv("dataset/breast-cancer-validate.dat", names=columns)
 
 # Pdpkt B
-malignant = train_data[train_data["Malignant/Benign"] == "M"]
-benign = train_data[train_data["Malignant/Benign"] == "B"]
-feature = "radius (mean)"
+def Histogram_B():
+  malignant = train_data[train_data["Malignant/Benign"] == "M"]
+  benign = train_data[train_data["Malignant/Benign"] == "B"]
+  feature = "radius (mean)"
 
-plt.figure(figsize=(8, 5))
-print(malignant[feature])
-plt.hist(malignant[feature], bins=20, alpha=0.7, label="Malignant", color="red")
-plt.hist(benign[feature], bins=20, alpha=0.7, label="Benign", color="blue")
+  plt.figure(figsize=(8, 5))
+  plt.hist(malignant[feature], bins=20, alpha=0.7, label="Malignant", color="red")
+  plt.hist(benign[feature], bins=20, alpha=0.7, label="Benign", color="blue")
 
-plt.xlabel(feature)
-plt.ylabel("Liczba pacjentów")
-plt.title(f"Histogram wartości cechy {feature}")
-plt.legend()
-plt.show()
+  plt.xlabel(feature)
+  plt.ylabel("Liczba pacjentów")
+  plt.title(f"Histogram wartości cechy {feature}")
+  plt.legend()
+  plt.grid(color='gray', linestyle='--', linewidth=0.5)
+  plt.yticks(np.arange(0, 20, 2))
+  plt.show()
+
+Histogram_B()
