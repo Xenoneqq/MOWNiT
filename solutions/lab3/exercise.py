9728ddb72fdaba4123afe0d90df487329e20e074
Xenoneqq <realfatboimlg@gmail.com>
2025-03-25 09:35:18 +0100

    Finished up to task D

--- solutions/lab3/exercise.py
+++ solutions/lab3/exercise.py
@@ -27,6 +27,9 @@ def interpolate(t, coeff, degree=0):
         return coeff[degree] + coeff[degree+1] * ((t-1940)/40)
     return coeff[degree] + ((t-1940)/40) * interpolate(t, coeff, degree+1)
 
+
+# PDPKT A + B + C
+
 x = [1900, 1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980]
 y = [76212168, 92228496, 106021537, 123202624, 132164569, 151325798, 179323175, 203302031, 226542199]
 
@@ -41,12 +44,12 @@ for n in range(4):
 print(f"Best condition number: {best_cond:.2e}")
 
 a = np.linalg.solve(best_matrix, y)
+#print(f"Współczynniki a: {a}")
 
 x_vals = np.arange(1900, 1991, 1)
 y_vals = [interpolate(t , a) for t in x_vals]
 
-print(f"Współczynniki a: {a}")
-
+# rysowanie wykresu na ekran (pdpkt c)
 plt.plot(x_vals, y_vals, label="Wielomian interpolacyjny", color='b')
 plt.scatter(x, y, color='r', label="Węzły interpolacyjne")
 plt.xlabel("Rok")
@@ -55,3 +58,12 @@ plt.title("Wielomian interpolacyjny")
 plt.legend()
 plt.grid(True)
 plt.show()
+
+# PDPKT D
+
+year_1990_true = 248709873
+year_predicted = interpolate(1990 , a)
+print("True 1990 value : " , year_1990_true)
+print("Predicted 1990 value : ", year_predicted)
+relative_population_error = (abs(year_1990_true - year_predicted) / year_1990_true) * 100
+print("Relative error of extrapolated population:", relative_population_error)
