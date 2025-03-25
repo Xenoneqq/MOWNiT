930fcc45a41964fd8fc5b73a272ecee22a95be6a
pswierzy <pswierzy@student.agh.edu.pl>
2025-03-25 19:38:05 +0100

    Finished tasks E-G

--- solutions/lab3/exercise.py
+++ solutions/lab3/exercise.py
@@ -15,11 +15,10 @@ def equation(t, j, n):
     return 1
 
 def create_matrix(X, n):
-    matrix = np.zeros(shape=(9, 9))
-    for i in range(9):
-        for j in range(9):
-            matrix[i][j] = equation(X[i], j+1, n)
-    matrix = np.matrix(matrix)
+    matrix = np.zeros((len(X), len(X)))
+    for i in range(len(X)):
+        for j in range(len(X)):
+            matrix[i, j] = equation(X[i], j+1, n)
     return matrix
 
 def interpolate(t, coeff, degree=0):
@@ -29,17 +28,18 @@ def interpolate(t, coeff, degree=0):
 
 
 # PDPKT A + B + C
-
 x = [1900, 1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980]
 y = [76212168, 92228496, 106021537, 123202624, 132164569, 151325798, 179323175, 203302031, 226542199]
 
 best_cond = math.inf
+best_n = 0
 for n in range(4):
     A = create_matrix(x, n)
     cond_number = np.linalg.cond(A)
     if cond_number < best_cond:
         best_cond = cond_number
         best_matrix = A
+        best_n = n
 
 print(f"Best condition number: {best_cond:.2e}")
 
@@ -49,21 +49,108 @@ a = np.linalg.solve(best_matrix, y)
 x_vals = np.arange(1900, 1991, 1)
 y_vals = [interpolate(t , a) for t in x_vals]
 
-# rysowanie wykresu na ekran (pdpkt c)
+plt.figure(figsize=(10, 6))
 plt.plot(x_vals, y_vals, label="Wielomian interpolacyjny", color='b')
 plt.scatter(x, y, color='r', label="Węzły interpolacyjne")
 plt.xlabel("Rok")
 plt.ylabel("Populacja")
 plt.title("Wielomian interpolacyjny")
 plt.legend()
-plt.grid(True)
+plt.grid()
 plt.show()
 
 # PDPKT D
-
 year_1990_true = 248709873
 year_predicted = interpolate(1990 , a)
-print("True 1990 value : " , year_1990_true)
-print("Predicted 1990 value : ", year_predicted)
+print("Prawdziwa wartość dla 1990 roku: " , year_1990_true)
+print("Przewidziana wartość dla 1990 roku: ", year_predicted)
 relative_population_error = (abs(year_1990_true - year_predicted) / year_1990_true) * 100
-print("Relative error of extrapolated population:", relative_population_error)
+print("Błąd względny ekstrapolacji dla roku 1990:", relative_population_error)
+
+# PDPKT E
+def lagrange_basis(x, i, t):
+    result = 1.0
+    for j in range(len(x)):
+        if j != i:
+            result *= (t - x[j]) / (x[i] - x[j])
+    return result
+
+def lagrange_interpolation(x, y, t):
+    result = 0.0
+    for i in range(len(x)):
+        result += y[i] * lagrange_basis(x, i, t)
+    return result
+
+y_vals = np.array([lagrange_interpolation(x,y,t) for t in x_vals])
+
+plt.figure(figsize=(10, 6))
+plt.plot(x_vals, y_vals, label='Wielomian Lagrange’a', color='blue')
+plt.scatter(x, y, color='red', label='Węzły interpolacji')
+plt.xlabel('Rok')
+plt.ylabel('Populacja')
+plt.title('Interpolacja populacji USA - Wielomian Lagrange’a')
+plt.legend()
+plt.grid()
+plt.show()
+
+# PDPKT F
+def divided_differences(x, y):
+    n = len(x)
+    coef = np.array(y, dtype=float)
+    for j in range(1, n):
+        for i in range(n-1, j-1, -1):
+            coef[i] = (coef[i] - coef[i-1]) / (x[i] - x[i-j])
+    return coef
+
+def newton_interpolation(x, coef, t):
+    n = len(coef)
+    result = coef[-1]
+    for i in range(n-2, -1, -1):
+        result = result * (t - x[i]) + coef[i]
+    return result
+
+coef = divided_differences(x, y)
+
+y_vals = np.array([newton_interpolation(x, coef, t) for t in x_vals])
+
+plt.figure(figsize=(10, 6))
+plt.plot(x_vals, y_vals, label='Wielomian Newtona', color='blue')
+plt.scatter(x, y, color='red', label='Węzły interpolacji')
+plt.xlabel('Rok')
+plt.ylabel('Populacja')
+plt.title('Interpolacja populacji USA - Wielomian Newtona')
+plt.legend()
+plt.grid()
+plt.show()
+
+# PDPKT G
+x = [1900, 1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980]
+y = [76000000, 92000000, 106000000, 123000000, 132000000, 151000000, 179000000, 203000000, 227000000]
+
+
+A_rounded = create_matrix(x, best_n)
+
+a_rounded = np.linalg.solve(best_matrix, y)
+
+y_vals = [interpolate(t , a_rounded) for t in x_vals]
+
+plt.figure(figsize=(10, 6))
+plt.plot(x_vals, y_vals, label="Wielomian interpolacyjny", color='b')
+plt.scatter(x, y, color='r', label="Węzły interpolacyjne")
+plt.xlabel("Rok")
+plt.ylabel("Populacja")
+plt.title("Wielomian interpolacyjny")
+plt.legend()
+plt.grid()
+plt.show()
+
+print("Współczynniki przed zaokrągleniem")
+print(a)
+print("Współczynniki po zaokrągleniu")
+print(a_rounded)
+
+year_predicted_rounded = interpolate(1990 , a_rounded)
+print("Prawdziwa wartość dla 1990 roku: " , year_1990_true)
+print("Przewidziana wartość dla 1990 roku: ", year_predicted_rounded)
+relative_population_error_rounded = (abs(year_1990_true - year_predicted_rounded) / year_1990_true) * 100
+print("Błąd względny ekstrapolacji dla roku 1990:", relative_population_error_rounded)
